7050d0f0807cd43565fc91b78b497d4dd2929247
yonashaile12 <yonashaile12@gmail.com>
2020-09-21 14:10:15 -0500

    VyTrackTask

--- src/test/java/com/cybertek/VyTrackTest/VyTrack.java
+++ src/test/java/com/cybertek/VyTrackTest/VyTrack.java
@@ -1,36 +1,40 @@
 package com.cybertek.VyTrackTest;
-
 import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 public class VyTrack {
-
-    @Test
+    WebDriver driver;
+    @BeforeMethod
     public void setupMethod() throws InterruptedException {
-
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
-
+        driver = WebDriverFactory.getDriver("chrome");
         driver.get("https://qa2.vytrack.com/");
-
-
+        driver.manage().window().maximize();
         WebElement emailInputBox = driver.findElement(By.id("prependedInput"));
-        emailInputBox.sendKeys("user123"); //user165
-
+        emailInputBox.sendKeys("user165"); //user165
         WebElement retrievePasswordButton = driver.findElement(By.name("_password"));
         retrievePasswordButton.sendKeys("UserUser123");
-
         driver.findElement(By.xpath("//button[@id='_submit']")).click();
         Thread.sleep(3000);
-
-        String actualTitle = driver.getTitle();
-        String expectedTitle = "Login";
-
-        Assert.assertEquals(actualTitle, expectedTitle);
-
-
+    }
+    @Test
+    public void test1_vehicle_odometerPage()throws InterruptedException{
+        WebElement fleetModule = driver.findElement(By.linkText("Fleet"));
+        fleetModule.click();
+        Thread.sleep(1000);
+        WebElement vehicleOdometer = driver.findElement(By.xpath("//span[contains(text(),'Vehicle Odometer')]"));
+        vehicleOdometer.click();
+        Thread.sleep(3000);
+        WebElement SelectVehicle = driver.findElement(By.xpath("//tr[6]//td[4]"));
+        SelectVehicle.click();
+        Thread.sleep(2000);
+        WebElement editButton = driver.findElement(By.xpath("//*[@id='container']/div[2]/div[1]/div/div/div[1]/div[1]/div[1]/a"));
+        editButton.click();
     }
 }
+
+

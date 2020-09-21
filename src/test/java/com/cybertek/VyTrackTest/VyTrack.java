01da313dd7548d7c3cd568e913283c677519ebb0
yonashaile12 <yonashaile12@gmail.com>
2020-09-21 10:31:33 -0500

    VyTrackTask

--- src/test/java/com/cybertek/VyTrackTest/VyTrack.java
+++ src/test/java/com/cybertek/VyTrackTest/VyTrack.java
@@ -5,10 +5,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class VyTrack {
 
-    public static void main(String[] args) throws InterruptedException {
+    @Test
+    public void setupMethod() throws InterruptedException {
 
         WebDriver driver = WebDriverFactory.getDriver("chrome");
 
@@ -24,26 +26,10 @@ public class VyTrack {
         driver.findElement(By.xpath("//button[@id='_submit']")).click();
         Thread.sleep(3000);
 
-        /*
-        driver.findElement(By.xpath("//span[@class='title title-level-1']")).click();
-        Thread.sleep(3000);
-
-        //driver.findElement(By.className("title title-level-1")).click();
-        //Thread.sleep(3000);
-
-        //driver.findElement(By.className("oro-subtitle")).click();
-         */
-
         String actualTitle = driver.getTitle();
         String expectedTitle = "Login";
 
         Assert.assertEquals(actualTitle, expectedTitle);
-
-
-
-
-
-
 
 
     }

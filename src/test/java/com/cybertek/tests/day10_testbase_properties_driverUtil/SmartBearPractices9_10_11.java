7290ad7f141bfd0141b8bc89d3542e6f44e68981
yonashaile12 <yonashaile12@gmail.com>
2020-10-04 13:51:48 -0500

    properties and smartBear Utils

--- src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/SmartBearPractices9_10_11.java
+++ src/test/java/com/cybertek/tests/day10_testbase_properties_driverUtil/SmartBearPractices9_10_11.java
@@ -3,16 +3,15 @@ package com.cybertek.tests.day10_testbase_properties_driverUtil;
 import com.cybertek.tests.Base.TestBase;
 import com.cybertek.utilities.BrowserUtils;
 import com.cybertek.utilities.SmartBearsUtilities;
-import com.cybertek.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.Properties;
 
 public class SmartBearPractices9_10_11 extends TestBase {
 /*
@@ -40,9 +39,21 @@ public class SmartBearPractices9_10_11 extends TestBase {
      */
 
     @Test
-    public void delete_order_task(){
+    public void delete_order_task() throws IOException {
 
-        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/webOrders/login.aspx");
+        //Creating properties objhect to be able to read properties file
+        Properties properties = new Properties();
+
+        //Opening the file in JVM memory using FileInputStream
+        FileInputStream file = new FileInputStream("configuration.properties");
+
+        //Loading the file into properties object
+        properties.load(file);
+
+        //--->URL FROM CONFIGURATION PROPERTIES
+        String url = properties.getProperty("smartbearUrl"); //returning url configuration properties
+
+        driver.get(url);
         SmartBearsUtilities.loginToSmartBear(driver);
         //locate the checkbox for 'Mark Smith' and click to it
         WebElement markSmithCheckBox = driver.findElement(By.xpath("//td[.='Mark Smith']/../td[1]"));

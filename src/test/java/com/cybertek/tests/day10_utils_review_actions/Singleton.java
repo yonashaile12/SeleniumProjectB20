1ab7284492a9b573ee28d5158abeb1af870e80d4
yonashaile12 <yonashaile12@gmail.com>
2020-10-06 19:37:16 -0500

    Singleton Practice

---
+++ src/test/java/com/cybertek/tests/day10_utils_review_actions/Singleton.java
@@ -0,0 +1,24 @@
+package com.cybertek.tests.day10_utils_review_actions;
+
+public class Singleton {
+
+    //Making the constructor private, so we limit creating objects from this class
+    private Singleton(){}
+
+    //Making the string private so that we can only reach this using getter method
+    private static String word;
+
+    //Creating a getter method to allow users to reach private String above
+    public static String getWord(){
+
+        if(word == null){
+            System.out.println("First time call. Word Object is null. " +
+                    "Assigning value to it now!");
+            word= "something";
+        }else{
+            System.out.println("Word already has value");
+        }
+
+        return word;
+    }
+}
---
+++ src/test/java/com/cybertek/tests/day10_utils_review_actions/SingletonPractices.java
@@ -0,0 +1,49 @@
+package com.cybertek.tests.day10_utils_review_actions;
+
+import org.testng.annotations.Test;
+
+public class SingletonPractices {
+/*
+if you run the whole class
+First time call. Word Object is null. Assigning value to it now!
+str1 = something
+Word already has value
+str1 = something
+Word already has value
+str1 = something
+
+
+Word already has value
+str1 = something
+ */
+    @Test
+    public void singleton_test1(){
+        /*
+        First time call. Word Object is null. Assigning value to it now!
+                str1 = something
+         */
+        String str1 = Singleton.getWord();
+        System.out.println("str1 = "+ str1);
+
+
+        String str2 = Singleton.getWord();
+        System.out.println("str1 = "+ str2);
+        /*
+        Word already has value
+        str2 = something
+         */
+
+        String str3 = Singleton.getWord();
+        System.out.println("str1 = "+ str3);
+        /*
+        Word already has value
+        str3 = something
+         */
+    }
+
+    @Test
+    public void singleton_test2(){
+        String str4 = Singleton.getWord();
+        System.out.println("str1 = "+ str4);
+    }
+}

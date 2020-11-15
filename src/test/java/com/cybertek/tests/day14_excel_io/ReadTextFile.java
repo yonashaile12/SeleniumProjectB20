5f00fe7e4c2765ffd20b471025de8359673e0e11
git config --global user.name <yonashaile12@gmail.com>
2020-11-15 09:46:00 -0600

    added file reader

---
+++ src/test/java/com/cybertek/tests/day14_excel_io/ReadTextFile.java
@@ -0,0 +1,29 @@
+package com.cybertek.tests.day14_excel_io;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ReadTextFile {
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String path = "shopping_list.txt";
+        // file's constructor requires path to the file as an argument
+        File file = new File(path);
+        //check if file exist
+        System.out.println(file.exists());
+        // we can read lines of file with scanner class
+        //System.in -for keyboard input
+        // file object - for reading file
+        Scanner scanner = new Scanner(file);
+
+        System.out.println(scanner.nextLine());
+        System.out.println(scanner.nextLine());
+
+        while(scanner.hasNext()){
+            System.out.println(scanner.nextLine());
+        }
+
+    }
+}
